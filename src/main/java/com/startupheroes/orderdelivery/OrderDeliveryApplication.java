d1f1deb2c095b3e8291bb5c6c9ddb702e3e221c8
merayman <musab.erayman@gmail.com>
2022-11-11 02:50:08 +0300

    first commit

---
+++ src/main/java/com/startupheroes/orderdelivery/OrderDeliveryApplication.java
@@ -0,0 +1,14 @@
+package com.startupheroes.orderdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderDeliveryApplication.class, args);
+	}
+
+
+}
